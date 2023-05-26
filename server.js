4ba2464fe8b0e8ff7339a82712d384b7459ea07e
kunalsachdeva <>
2023-05-26 13:13:29 +0530

    localhost to 0.0.0.0

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT,'localhost', () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
